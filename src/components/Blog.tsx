747e273e3ea4ed8abad4f21d7c92076ca034b6a9
GMTStudiotech2024 <153701583+GMTStudiotech2024@users.noreply.github.com>
2024-06-17 13:16:18 +0800

    commit

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -5,7 +5,7 @@ import blogImage3 from '../assets/images/blog3.png';
 
 const blogPosts = [
   { image: blogImage1, title: "MAZS AI", excerpt: "GMTStudio Ai workspace provide The service of Artificial intelligence " },
-  { image: blogImage2, title: "Blog Post Two", excerpt: "A brief excerpt of the second blog post." },
+  { image: blogImage2, title: "Theta Social media Application", excerpt: "The newly designed website application, Which were recently updated." },
   { image: blogImage3, title: "Blog Post Three", excerpt: "A brief excerpt of the third blog post." }
 ];
 
@@ -14,7 +14,7 @@ const Blog: React.FC = () => {
     <section id="blog" className="py-10 bg-white dark:bg-gray-800">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <h2 className="text-4xl font-bold text-gray-900 dark:text-white mb-8 text-center">Our Blog</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8" data-aos="fade-up">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8" data-aos="zoom-out-up">
           {blogPosts.map((post, index) => (
             <div key={index} className="rounded-lg shadow-lg overflow-hidden transform transition-transform hover:scale-105 duration-300 ease-in-out">
               <img src={post.image} alt={post.title} className="w-full h-48 object-cover" />
