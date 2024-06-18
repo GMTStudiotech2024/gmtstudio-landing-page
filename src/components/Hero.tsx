88fc37bbeca0a4dee1873c3cdcb5411cc327756b
GMTStudiotech2024 <153701583+GMTStudiotech2024@users.noreply.github.com>
2024-06-18 10:04:16 +0800

    commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,14 +15,7 @@ const Hero: React.FC = () => {
           <p className="text-base mb-6 text-gray-700 dark:text-gray-300">
             Welcome to GMTStudio Tech, your destination for innovative software solutions. We specialize in web and mobile app development, offering custom software tailored to your needs. Our expert team is committed to delivering high-quality products that drive business growth and success. Explore how we can transform your digital landscape.
           </p>
-          <div className="space-x-4">
-            <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 dark:bg-green-500 dark:hover:bg-green-600">
-              Get started
-            </button>
-            <button className="text-blue-500 border border-blue-500 px-4 py-2 rounded hover:bg-blue-500 hover:text-white dark:border-green-500 dark:hover:bg-green-500 dark:text-green-500 dark:hover:text-white">
-              Learn more
-            </button>
-          </div>
+
         </div>
         <div className="lg:w-1/2 w-full flex justify-center" data-aos="fade-left">
           <img src={screenshot} alt="Screenshot" className="rounded-lg shadow-lg w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl" />
