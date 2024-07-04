bb670e6ce68ff4e6f3a57140a784e74aa91d5379
GMTStudiotech2024 <153701583+GMTStudiotech2024@users.noreply.github.com>
2024-07-04 16:40:32 +0800

    commit

--- src/components/OurProjects.tsx
+++ src/components/OurProjects.tsx
@@ -4,14 +4,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight, faExternalLinkAlt, faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
 import projectImage1 from '../assets/images/blog4.png';
 import projectImage2 from '../assets/images/blog5.png';
-import projectImage3 from '../assets/images/1.png';
+import projectImage3 from '../assets/images/Story.jpg';
 
 const projects = [
   {
-    title: 'GMTStudio SyncTask OS',
-    description: 'We call our website the Web OS because it makes it easier to announce new updates.',
+    title: 'GMTStudio Story Vending Maching',
+    description: 'New Ideas in GMTStudio',
     image: projectImage3,
-    link: '/error',
+    link: '/BlogPage3',
     icon: faExclamationCircle
   },
   {
