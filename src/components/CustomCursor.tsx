493ce26903c3d208acc312b124fd9d443cf8080f
GMTStudiotech2024 <153701583+GMTStudiotech2024@users.noreply.github.com>
2024-07-09 14:30:49 +0800

    commit

--- src/components/CustomCursor.tsx
+++ src/components/CustomCursor.tsx
@@ -5,6 +5,9 @@ const CustomCursor: React.FC = () => {
   const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
   const [click, setClick] = useState(false);
   const [hover, setHover] = useState(false);
+  const [cursorLine, setCursorLine] = useState(false);
+  const [cursorButton, setCursorButton] = useState(false);
+  const [cursorInput, setCursorInput] = useState(false);
 
   useEffect(() => {
     const moveCursor = (e: MouseEvent) => {
@@ -15,14 +18,26 @@ const CustomCursor: React.FC = () => {
     const handleMouseUp = () => setClick(false);
 
     const handleMouseOver = (e: MouseEvent) => {
-      if ((e.target as Element).matches('a, button')) {
+      if ((e.target as Element).matches('button')) {
+        setCursorButton(true);
+      } else if ((e.target as Element).matches('a')) {
         setHover(true);
+      } else if ((e.target as Element).matches('p, span, input, textarea')) {
+        setCursorLine(true);
+      } else if ((e.target as Element).matches('input, textarea')) {
+        setCursorInput(true);
       }
     };
 
     const handleMouseOut = (e: MouseEvent) => {
-      if ((e.target as Element).matches('a, button')) {
+      if ((e.target as Element).matches('button')) {
+        setCursorButton(false);
+      } else if ((e.target as Element).matches('a')) {
         setHover(false);
+      } else if ((e.target as Element).matches('p, span, input, textarea')) {
+        setCursorLine(false);
+      } else if ((e.target as Element).matches('input, textarea')) {
+        setCursorInput(false);
       }
     };
 
@@ -43,7 +58,7 @@ const CustomCursor: React.FC = () => {
 
   return (
     <div
-      className={`custom-cursor ${click ? 'click' : ''} ${hover ? 'hover' : ''}`}
+      className={`custom-cursor ${click ? 'click' : ''} ${hover ? 'hover' : ''} ${cursorLine ? 'cursor--text' : ''} ${cursorButton ? 'cursor-button' : ''} ${cursorInput ? 'cursor-input' : ''}`}
       style={{ left: `${cursorPos.x}px`, top: `${cursorPos.y}px` }}
     />
   );
