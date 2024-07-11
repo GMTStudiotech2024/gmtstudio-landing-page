746e2537f3764e98ab401b0b81705f66f0b20e00
alstonduck <alstonduck@gmail.com>
2024-07-11 04:49:00 +0000

    commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,8 +6,8 @@ const Footer: React.FC = () => {
       <div className="container mx-auto px-4 md:px-8 lg:px-16">
         <div className="flex flex-col md:flex-row items-center justify-between">
           <div className="text-center md:text-left mb-4 md:mb-0">
-            <img src="path-to-your-logo.png" alt="GMTStudio Logo" className="mx-auto md:mx-0 w-32 h-auto mb-4 md:mb-0" />
-            <p>&copy; 2024 GMTStudio TECH. All rights reserved. Website Version: 💻v1.5</p>
+            <img src="../public/favicon.ico" alt="GMTStudio Logo" className="mx-auto md:mx-0 w-32 h-auto mb-4 md:mb-0" />
+            <p>&copy; 2024 GMTStudio TECH. All rights reserved. Website Version: 💻v1.6</p>
           </div>
           <div className="footer-links flex flex-wrap justify-center md:justify-end mt-4 md:mt-0 space-x-4">
             <a href="#hero" className="footer-link">Home</a>
